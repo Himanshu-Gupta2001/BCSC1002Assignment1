0e98dfbab339396406cab540e7f908b9be4eea93
Himanshu Gupta <himanshu.gupta_cs19@gla.ac.in>
2020-08-30 15:27:01 +0530

    add private fields for operation in FrontDesk.java class

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -7,5 +7,10 @@
 package execution;
 
 public class FrontDesk {
+    private static final int ISSUE_A_NEW_BOOK_FOR_ME = 1;
+    private static final int RETURN_A_PREVIOUSLY_ISSUED_BOOK_FOR_ME = 2;
+    private static final int SHOW_ME_ALL_MY_ISSUES_BOOKS = 3;
+    private static final int EXIT = 4;
+
 
 }

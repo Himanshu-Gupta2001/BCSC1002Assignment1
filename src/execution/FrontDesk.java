b3df55989044ecc1be0c2081b354e713db66e957
Himanshu Gupta <himanshu.gupta_cs19@gla.ac.in>
2020-08-30 14:47:52 +0530

    initial commit in FrontDesk.java

--- src/execution/FrontDesk.java
+++ src/execution/FrontDesk.java
@@ -1,13 +1,10 @@
-/*  Created by IntelliJ IDEA.
- *  User: Divyansh Bhardwaj (dbc2201)
- *  Date: 21/08/20
- *  Time: 3:50 PM
- *  File Name : FrontDesk.java
- * */
+/* Created by IntelliJ IDEA.
+ *  Author: Himanshu Gupta (191500345)
+ *  Date: 29/8/2020
+ *  Time: 1:02 AM
+ *  File: FrontDesk.java
+ */
 package execution;
 
 public class FrontDesk {
-    public static void main(String[] args) {
-
-    }
 }

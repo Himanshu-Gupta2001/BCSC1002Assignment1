cac21c88ab5f9f719264e2849753da564622b7c6
Himanshu Gupta <himanshu.gupta_cs19@gla.ac.in>
2020-08-30 10:53:48 +0530

    add field to store the name of book in Book.java

--- src/definitions/Book.java
+++ src/definitions/Book.java
@@ -8,4 +8,15 @@
 package definitions;
 
 public class Book {
+
+    //Components of a definition class in Java.
+    // 1. private fields.
+    // 2. getter methods for private fields.
+    // 3. setter methods for private fields.
+    // 4. Constructors for initializing the fields of an object.
+    // 5. the toString() method.
+    // 6. the equals() and the hashCode() methods.
+
+    private String bookName;
+
 }
